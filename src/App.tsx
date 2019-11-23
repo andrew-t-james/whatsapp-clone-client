02b5a68811ea40860f8f09c1fc442625e8e286a4
Andrew James <ajames20@gmail.com>
2019-11-23 15:48:09 -0600

    Add react-router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import ChatsListScreen from './components/ChatListScreen'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+// import ChatRoomScreen from './components/ChatRoomScreen'
+import ChatListScreen from './components/ChatListScreen'
+
+const redirectToChats = (): JSX.Element => <Redirect to="/chats" />
 
 const App: React.FC = () => {
   return (
-    <div>
-      <ChatsListScreen />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/chats" component={ChatListScreen} />
+        {/* <Route exact path="/chats/:chatId" component={ChatRoomScreen} /> */}
+      </Switch>
+      <Route exact path="/" render={redirectToChats} />
+    </BrowserRouter>
   )
 }
 
