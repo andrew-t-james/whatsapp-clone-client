20a0da7188715e03061092c30a8bb7b079e17855
Andrew James <ajames20@gmail.com>
2019-11-23 15:48:25 -0600

    Add react-router and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
-// import ChatRoomScreen from './components/ChatRoomScreen'
+import ChatRoomScreen from './components/ChatRoomScreen'
 import ChatListScreen from './components/ChatListScreen'
 
 const redirectToChats = (): JSX.Element => <Redirect to="/chats" />
@@ -10,7 +10,7 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/chats" component={ChatListScreen} />
-        {/* <Route exact path="/chats/:chatId" component={ChatRoomScreen} /> */}
+        <Route exact path="/chats/:chatId" component={ChatRoomScreen} />
       </Switch>
       <Route exact path="/" render={redirectToChats} />
     </BrowserRouter>
