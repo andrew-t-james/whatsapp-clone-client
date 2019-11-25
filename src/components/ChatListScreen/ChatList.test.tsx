2c0a2540bc0da1e92c5300f4c1e869876211d7b9
Andrew James <ajames20@gmail.com>
2019-11-25 07:48:24 -0600

    Add tests for ChatList

--- src/components/ChatListScreen/ChatList.test.tsx
+++ src/components/ChatListScreen/ChatList.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
 import {
   cleanup,
   render,
@@ -6,38 +7,56 @@ import {
   wait,
   waitForDomChange
 } from '@testing-library/react'
-import ChatList from './ChatList'
 import { createBrowserHistory } from 'history'
+import { mockApolloClient } from '../../test-helpers'
+import ChatList, { getChatsQuery } from './ChatList'
 
 describe('ChatsList', () => {
   afterEach(() => {
+    cleanup()
     delete window.location
+
     window.location = {
       href: '/'
     }
   })
-  test('renders fetched chats data', async () => {
-    fetchMock.mockResponseOnce(
-      JSON.stringify({
-        data: {
-          chats: [
-            {
-              id: 1,
-              name: 'Foo Bar',
-              picture: 'https://localhost:4000/picture.jpg',
-              lastMessage: {
+
+  it('renders fetched chats data', async () => {
+    const client = mockApolloClient([
+      {
+        request: { query: getChatsQuery },
+        result: {
+          data: {
+            chats: [
+              {
+                __typename: 'Chat',
                 id: 1,
-                content: 'Hello',
-                createdAt: new Date('1 Jan 2019 GMT')
+                name: 'Foo Bar',
+                picture: 'https://localhost:4000/picture.jpg',
+                lastMessage: {
+                  __typename: 'Message',
+                  id: 1,
+                  content: 'Hello',
+                  createdAt: new Date('1 Jan 2019 GMT')
+                }
               }
-            }
-          ]
+            ]
+          }
         }
-      })
-    )
+      }
+    ])
+
+    const history = createBrowserHistory()
+
     {
-      const { container, getByTestId } = render(<ChatList history={history} />)
+      const { container, getByTestId } = render(
+        <ApolloProvider client={client}>
+          <ChatList history={history} />
+        </ApolloProvider>
+      )
+
       await waitForDomChange({ container })
+
       expect(getByTestId('name')).toHaveTextContent('Foo Bar')
       expect(getByTestId('picture')).toHaveAttribute(
         'src',
@@ -48,32 +67,44 @@ describe('ChatsList', () => {
     }
   })
 
-  test('should navigate to the target chat room on chat item click', async () => {
-    fetchMock.mockResponseOnce(
-      JSON.stringify({
-        data: {
-          chats: [
-            {
-              id: 1,
-              name: 'Foo Bar',
-              picture: 'https://localhost:4000/picture.jpg',
-              lastMessage: {
+  it('should navigate to the target chat room on chat item click', async () => {
+    const client = mockApolloClient([
+      {
+        request: { query: getChatsQuery },
+        result: {
+          data: {
+            chats: [
+              {
+                __typename: 'Chat',
                 id: 1,
-                content: 'Hello',
-                createdAt: new Date('1 Jan 2019 GMT')
+                name: 'Foo Bar',
+                picture: 'https://localhost:4000/picture.jpg',
+                lastMessage: {
+                  __typename: 'Message',
+                  id: 1,
+                  content: 'Hello',
+                  createdAt: new Date('1 Jan 2019 GMT')
+                }
               }
-            }
-          ]
+            ]
+          }
         }
-      })
-    )
+      }
+    ])
 
     const history = createBrowserHistory()
 
     {
-      const { container, getByTestId } = render(<ChatList history={history} />)
+      const { container, getByTestId } = render(
+        <ApolloProvider client={client}>
+          <ChatList history={history} />
+        </ApolloProvider>
+      )
+
       await waitForDomChange({ container })
+
       fireEvent.click(getByTestId('chat'))
+
       await wait(() => expect(history.location.pathname).toEqual('/chats/1'))
     }
   })
